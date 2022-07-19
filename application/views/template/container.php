f372ffe1089130db41287a91a2c41a11d087d11d
constantiusleo <constantiusleo@student.ub.ac.id>
2022-07-19 15:21:44 +0700

    title page

--- application/views/template/container.php
+++ application/views/template/container.php
@@ -8,7 +8,7 @@
   <meta name="description" content="">
   <meta name="author" content="">
   <link href="<?php echo base_url('assets/img/logo/aiia-logo.png'); ?>" rel="icon">
-  <title>RuangAdmin - Dashboard</title>
+  <title>RFID - Dashboard</title>
   <link href="<?php echo base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
   <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
   <!-- Select2 -->
