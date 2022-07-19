e41771cc17cb1c91b0a16362663a081f6b14d7a0
constantiusleo <constantiusleo@student.ub.ac.id>
2022-07-19 13:34:59 +0700

    Add logo and general improvement

--- application/views/template/container.php
+++ application/views/template/container.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <link href="<?php echo base_url('assets/img/logo/logo.png'); ?>" rel="icon">
+  <link href="<?php echo base_url('assets/img/logo/aiia-logo.png'); ?>" rel="icon">
   <title>RuangAdmin - Dashboard</title>
   <link href="<?php echo base_url('assets/vendor/fontawesome-free/css/all.min.css'); ?>" rel="stylesheet" type="text/css">
   <link href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet" type="text/css">
