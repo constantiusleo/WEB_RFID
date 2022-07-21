d9104a753f7f330c851fb4e7304323707e0a1590
constantiusleo <constantiusleo@student.ub.ac.id>
2022-07-21 11:05:41 +0700

    sync card

--- application/controllers/PilihCustomer.php
+++ application/controllers/PilihCustomer.php
@@ -13,6 +13,19 @@ class PilihCustomer extends CI_Controller
 
     public function index()
     {
+        $data['data'] = $this->User_m->get()->result();
+		$data['total'] = $this->User_m->count_row();
+		$data['total_available'] = $this->User_m->count_available();
+		$data['total_delivery'] = $this->User_m->count_in_delivery();
+		$data['palet_biru'] = $this->User_m->count_palet_biru();
+		$data['palet_hijau'] = $this->User_m->count_palet_hijau();
+		$data['box_331'] = $this->User_m->count_box_331();
+		$data['palet_biru_available'] = $this->User_m->count_palet_biru_available();
+		$data['palet_hijau_available'] = $this->User_m->count_palet_hijau_available();
+		$data['box_331_available'] = $this->User_m->count_box_331_available();
+		$data['palet_biru_in_delivery'] = $this->User_m->count_palet_biru_in_delivery();
+		$data['palet_hijau_in_delivery'] = $this->User_m->count_palet_hijau_in_delivery();
+		$data['box_331_in_delivery'] = $this->User_m->count_box_331_in_delivery();
         $data['data'] = $this->User_m->distinct_type()->result();
         $data['content'] = 'pilihCustomer';
         $this->load->view($this->layout, $data);
--- application/views/pilihCustomer.php
+++ application/views/pilihCustomer.php
@@ -9,69 +9,69 @@
         </ol>
     </div>
     <div class="row mb-3">
-        <!-- PALET BIRU Available Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <div class="row align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Biru</div>
-                            <div class="h5 mb-0 font-weight-bold text-success mr-2">192</div>
-                        </div>
-                        <div class="col-auto">
-                            <img src="assets/img/house-door.svg" alt="Bootstrap" width="32" height="32">
-                        </div>
-                    </div>
-                </div>
+    <!-- PALET BIRU Available Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="row align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Biru</div>
+              <div class="h5 mb-0 font-weight-bold text-success mr-2"><?php echo $palet_biru_available ?></div>
             </div>
-        </div>
-        <!-- PALET HIJAU Available Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Hijau</div>
-                            <div class="h5 mb-0 font-weight-bold text-success mr-2">193</div>
-                        </div>
-                        <div class="col-auto">
-                            <img src="assets/img/house-door.svg" alt="Bootstrap" width="32" height="32">
-                        </div>
-                    </div>
-                </div>
+            <div class="col-auto">
+              <img src="assets/img/house-door.svg" alt="Bootstrap" width="32" height="32">
             </div>
+          </div>
         </div>
-        <!-- PALET BIRU Delivery Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Biru</div>
-                            <div class="h5 mb-0 mr-3 font-weight-bold text-warning mr-2">120</div>
-                        </div>
-                        <div class="col-auto">
-                            <img src="assets/img/truck.svg" alt="Bootstrap" width="32" height="32">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- PALET HIJAU Delivery  Card Example -->
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card h-100">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Hijau</div>
-                            <div class="h5 mb-0 font-weight-bold text-warning mr-2">133</div>
-                        </div>
-                        <div class="col-auto">
-                            <img src="assets/img/truck.svg" alt="Bootstrap" width="32" height="32">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-</div>
+    <!-- PALET HIJAU Available Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Hijau</div>
+              <div class="h5 mb-0 font-weight-bold text-success mr-2"><?php echo $palet_hijau_available ?></div>
+            </div>
+            <div class="col-auto">
+              <img src="assets/img/house-door.svg" alt="Bootstrap" width="32" height="32">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- PALET BIRU Delivery Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Biru</div>
+              <div class="h5 mb-0 mr-3 font-weight-bold text-warning mr-2"><?php echo $palet_biru_in_delivery ?></div>
+            </div>
+            <div class="col-auto">
+              <img src="assets/img/truck.svg" alt="Bootstrap" width="32" height="32">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- PALET HIJAU Delivery  Card Example -->
+    <div class="col-xl-3 col-md-6 mb-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="text-xs font-weight-bold text-uppercase mb-1">Palet Hijau</div>
+              <div class="h5 mb-0 font-weight-bold text-warning mr-2"><?php echo $palet_hijau_in_delivery ?></div>
+            </div>
+            <div class="col-auto">
+              <img src="assets/img/truck.svg" alt="Bootstrap" width="32" height="32">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
